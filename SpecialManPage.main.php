77626c6ffe5a7d8fbefa8626a17d2838a2223ac4
root <root@cloud4.hypertwins.net>
2018-03-14 00:17:43 +0000

    Now works with latest Ferreteria API changes.

--- SpecialManPage.main.php
+++ SpecialManPage.main.php
@@ -1,91 +1,85 @@
 <?php
 /*
-if (!defined('LIBMGR')) {
-    require('libmgr.php');
-}
-clsLibMgr::Add('menus',		KFP_MW_LIB.'/menu.php',__FILE__,__LINE__);
-clsLibMgr::Add('richtext',	KFP_MW_LIB.'/richtext.php',__FILE__,__LINE__);
-clsLibMgr::Load('menus'		,__FILE__,__LINE__);
-clsLibMgr::Load('richtext'	,__FILE__,__LINE__);
+  PURPOSE: MediaWiki SpecialPage to display manpages for requested items
+  HISTORY:
+    2018-03-12 significant updating
 */
-clsLibrary::Load_byName('ferreteria.mw.1');
 
-class SpecialManPage extends SpecialPageApp {
-//=======
-// STATIC
+// for the URL parser:
+define('KS_CHAR_PATH_SEP','/');
+define('KS_CHAR_URL_ASSIGN',':');
 
-//=======
-// DYNAMIC
+class SpecialManPage extends \SpecialPage {
+    use \ferreteria\mw\tSpecialPage;
 
-  protected $args;
-
-  public function __construct() {
-	global $wgOut, $wgMessageCache;
-
+    public function __construct() {
 	parent::__construct( 'ManPage' );
-	//$this->includable( true );	// 2015-02-12 no longer defined
-  }
-  function execute( $par ) {
+    }
+    protected function Go() {
 	global $wgUser;
 
 	$this->setHeaders();
-	$this->GetArgs($par);
 	if ($wgUser->isAllowed('editinterface')) {
-		$this->doAdmin();
+	    $this->doAdmin();
 	} else {
-		$this->doUser();
+	    $this->doUser();
 	}
-  }
-  public function doAdmin() {
+    }
+    /*----
+      PURPOSE: do stuff that only admins are allowed to do
+	For now, admins have no special powers, so just call doUser().
+    */
+    public function doAdmin() {
+	$this->doUser();
+    }
+    /*----
+	  PURPOSE: do only stuff that regular users are allowed to do
+    */
+    public function doUser() {
 	global $wgOut;
-/*
-	PURPOSE: do stuff that only admins are allowed to do
-	  For now, admins have no special powers, so just call doUser().
-*/
-      $this->doUser();
-// display menu
-  }
-  /*----
-	PURPOSE: do only stuff that regular users are allowed to do
-  */
-  public function doUser() {
-	global $wgOut;
-	$page = nzArr($this->args,'page');
-	$sect = nzArr($this->args,'section');
+
+	$oPathIn = \fcApp::Me()->GetKioskObject()->GetInputObject();
+	
+	// 2018-03-12 this will need updating
+	$page = $oPathIn->GetString('page');
+	$sect = $oPathIn->GetString('section');
 
 	if (is_null($page)) {
-		$this->ShowHelp();
+	    $this->ShowHelp();
 	} else {
 
-		$carg = $page;
-		if (!is_null($sect)) {
-		    $carg = $sect.' '.$carg;
-		}
+	    $carg = $page;
+	    if (!is_null($sect)) {
+		$carg = $sect.' '.$carg;
+	    }
 
-		$txtCmd = 'man --html=cat '.$carg;
-		exec($txtCmd,$arCmdOut,$intCmdStat);
-		$intLines = count($arCmdOut);
-		$wgOut->AddHTML('<small>');
-		$wgOut->AddHTML('<b>Page</b>: '.$page.' | ');
-		$wgOut->AddHTML('<b>Status</b>: '.$intCmdStat.'| ');
-		$wgOut->AddHTML('<b>Lines</b>: '.$intLines.' | ');
-		$wgOut->AddHTML('<b>Command</b>: '.$txtCmd.'</small><hr>');
-		foreach($arCmdOut as $txtLine) {
-		    $txtLine = str_replace('<small>',NULL,$txtLine);
-		    $txtLine = str_replace('</small>',NULL,$txtLine);
-		    $txtLine = str_replace('<big>',NULL,$txtLine);
-		    $txtLine = str_replace('</big>',NULL,$txtLine);
-		    $wgOut->AddHTML($txtLine."\n");
-		}
+	    $txtCmd = 'man --html=cat '.$carg;
+	    exec($txtCmd,$arCmdOut,$intCmdStat);
+	    $intLines = count($arCmdOut);
+	    $wgOut->AddHTML(
+	      '<small>'
+	      .'<b>Page</b>: '.$page.' | '
+	      .'<b>Status</b>: '.$intCmdStat.'| '
+	      .'<b>Lines</b>: '.$intLines.' | '
+	      .'<b>Command</b>: '.$txtCmd.'</small><hr>'
+	      );
+	    foreach($arCmdOut as $txtLine) {
+		$txtLine = str_replace('<small>',NULL,$txtLine);
+		$txtLine = str_replace('</small>',NULL,$txtLine);
+		$txtLine = str_replace('<big>',NULL,$txtLine);
+		$txtLine = str_replace('</big>',NULL,$txtLine);
+		$wgOut->AddHTML($txtLine."\n");
+	    }
 	}
-  }
-  public function ShowHelp() {
+    }
+    public function ShowHelp() {
 	global $wgOut;
 
 	$out = '<b>Format</b>: '.$this->BaseURL().'/page:<i>man-page-name</i>';
 	$wgOut->AddHtml($out);
-  }
+    }
 }
+/*
 function nzArr($iArr=NULL,$iKey,$iDefault=NULL) {
     if (is_null($iArr)) {
 	return $iDefault;
@@ -97,3 +91,4 @@ function nzArr($iArr=NULL,$iKey,$iDefault=NULL) {
 	}
     }
 }
+*/
